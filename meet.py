36223c67572add381c0515552fab3e0467a90ac6
meet <meets2534@gmail.com>
2022-06-08 16:03:35 +0530

    first commit

---
+++ meet.py
@@ -0,0 +1,11 @@
+import json
+x = {
+    "name": "Meet",
+    "age": 23,
+    "role":"B.Tech",
+    "email":"meets2534@gmail.com",
+    "phone-number":9867539604,
+    "salary": 25000,
+}
+v = json.dumps(x)
+print(x)
